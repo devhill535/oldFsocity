fa513f64a9c299ccced0fd7adf74f35ab7185e07
Halat <88115270+Halat69@users.noreply.github.com>
2022-03-22 11:45:26 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,18 +18,20 @@ module.exports = {
     .setThumbnail(`https://cdn.discordapp.com/attachments/938824959625662554/955523441107300402/music-logo-design.jpg`)
     .setTitle(`**ITuneR Commands**`)
     .setDescription(`
-**🔰┊Info Commands**
+**<a:840629271147839527:955523206712811530>┊Info Commands**
 invite ,support ,about ,ping ,uptime ,bot
-**🎵┊Music Commands**
+
+**<a:864935810192769074:955522901635899456>┊Music Commands**
 play , skip , skipto , stop , volume , nowplaying , shuffle , search , resume ,
 remove , queue , filter , loop , lyrics
-**🔗┊Links**
+
+**<a:861607472774250546:955522901459763240>┊Links**
 [Support](https://discord.gg/vCyZr3vyjY) - [Invite](https://discord.com/api/oauth2/authorize?client_id=954070757472890890&permissions=8&scope=bot)
 `)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("RANDOM");
-   message.react("✅")
+   message.react("<a:794514869579153408:955522901463941161>")
     return message.channel.send(helpEmbed).catch(console.error);
 
   }
