dd264a81aaadcb095b0983272babfb5083d76567
Halat <88115270+Halat69@users.noreply.github.com>
2022-03-22 12:00:39 +0300

    Update support.js

--- Music/support.js
+++ Music/support.js
@@ -10,7 +10,7 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
     .setDescription(` 
 \`link server\`
-**[ support ](https://discord.gg/dTNKbPXw9Y)**   
+**[ support ](https://discord.gg/vCyZr3vyjY)**   
  `)
     
     helpEmbed.setTimestamp();
