ec7b0cc03d1f135a405bb530f48e7f1854034c16
Halat <88115270+Halat69@users.noreply.github.com>
2022-03-22 11:52:54 +0300

    Update invite.js

--- Music/invite.js
+++ Music/invite.js
@@ -12,7 +12,7 @@ module.exports = {
   cooldown: 4,
   edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
   execute(message, args, client) {
-    message.reply(new MessageEmbed().setColor("BLACK").setTitle("**link bot**").setDescription("https://discord.com/api/oauth2/authorize?client_id=867208331659706379&permissions=0&scope=bot")
+    message.reply(new MessageEmbed().setColor("RANOM").setTitle("**link bot**").setDescription("https://discord.com/api/oauth2/authorize?client_id=954070757472890890&permissions=8&scope=bot")
     .setFooter("Invite Bot", ""));
 
   }
